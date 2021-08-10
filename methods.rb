835e09ca88633168dac6f58d9b9a10bee8b99102
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-08-09 21:56:00 -0700

    commit tree 6208952f366bc372fb9a1ed441fc1c8cdc2ce4ea

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def my_method(param)
+    puts "running my_method"
+    return param + 1 #the last line of a method will be the return value
+end
+
+def say_hi (name = "rubist") 
+    puts "hi there #{name}"
+end
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+def greet name
+    puts "Hello, #{name}!"
+end
+def add num1, num2
+    num1+num2
+end
+def halve num1
+    if num1.class != Integer 
+    return nil
+    end
+    num1/2
+end
